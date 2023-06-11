8c745cd3682d1deadf6e4301c60157159c86be92
Karol Pietryka <karol.pietryka11@gmail.com>
2023-06-11 07:04:30 +0200

    [Fix]Fix typos

--- front/run_kp_port.bat
+++ front/run_kp_port.bat
@@ -1,9 +1,10 @@
 @echo off
 
 if "%1"=="build" (
-  docker build -t kp_port:%2 .
+  ng build --prod
+  docker build -t kp_cv:%2 .
 ) else if "%1"=="run" (
-  docker run -p 80:80 kp_port:%2
+  docker run -p 80:80 kp_cv:%2
 ) else (
   echo "Invalid argument. Please provide either 'build' or 'run'."
 )
