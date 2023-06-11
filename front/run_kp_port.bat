09ebde23bfd61735417b4c5e28bf0ad6d1622fba
Karol Pietryka <karol.pietryka11@gmail.com>
2023-06-11 07:13:32 +0200

    [Fix] Fix deploy files on typos and naming

--- front/run_kp_port.bat
+++ front/run_kp_port.bat
@@ -2,9 +2,9 @@
 
 if "%1"=="build" (
   ng build --prod
-  docker build -t kp_cv:%2 .
+  docker build -t kp_port:%2 .
 ) else if "%1"=="run" (
-  docker run -p 80:80 kp_cv:%2
+  docker run -p 80:80 kp_port:%2
 ) else (
   echo "Invalid argument. Please provide either 'build' or 'run'."
 )
