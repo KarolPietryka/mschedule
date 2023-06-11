6a593749d71dd627243ce4ea6151dedf56f68b95
Karol Pietryka <karol.pietryka11@gmail.com>
2023-06-11 17:52:37 +0200

    [Add]Add icon

--- front/run_kp_port.bat
+++ front/run_kp_port.bat
@@ -3,8 +3,9 @@
 if "%1"=="build" (
   ng build --prod
   docker build -t kp_port:%2 .
+  @REM use d-c if next cont needed. d-c already in repo
 ) else if "%1"=="run" (
-  docker run -p 80:80 -d kp_port:%2
+  docker run -p 80:80 -d --name kp_port kp_port:%2
 ) else (
   echo "Invalid argument. Please provide either 'build' or 'run'."
 )
