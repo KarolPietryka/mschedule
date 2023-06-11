72b3a64eb0cc93944dc52c959a470a97dddaee45
Karol Pietryka <karol.pietryka11@gmail.com>
2023-06-11 07:59:06 +0200

    [Improve] Impr on bat file

--- front/run_kp_port.bat
+++ front/run_kp_port.bat
@@ -4,7 +4,7 @@ if "%1"=="build" (
   ng build --prod
   docker build -t kp_port:%2 .
 ) else if "%1"=="run" (
-  docker run -p 80:80 kp_port:%2
+  docker run -p 80:80 -d kp_port:%2
 ) else (
   echo "Invalid argument. Please provide either 'build' or 'run'."
 )
